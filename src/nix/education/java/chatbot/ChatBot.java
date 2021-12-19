5ee2cfddea5c7f337551d1fc00a6f43b12a9f8a5
Lavrov529st <m.o.lavrov@student.khai.edu>
2021-12-19 18:35:52 +0200

    ChatBot 2-nd stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -11,5 +11,7 @@ public class ChatBot {
         System.out.println();
         System.out.println("Hello! My name is  Vasia");
         System.out.println("I was created in " + localDate);
+        Scanner s11 = new Scanner(System.in);
+        System.out.println("What a great name you have, " + s11.nextLine());
     }
 }
