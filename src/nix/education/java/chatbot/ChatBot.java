41ffbd54a1bef6ffc504f63d4069284e2ca6fda5
Lavrov529st <m.o.lavrov@student.khai.edu>
2021-12-19 18:34:57 +0200

    ChatBot 1-st stage

---
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -0,0 +1,15 @@
+package nix.education.java.chatbot;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+
+        LocalDate localDate = LocalDate.now();
+
+        System.out.println();
+        System.out.println("Hello! My name is  Vasia");
+        System.out.println("I was created in " + localDate);
+    }
+}
